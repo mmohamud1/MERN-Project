7056ba3081762d4f299a8f8ddc5eaa10f4b59cef
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-10-04 16:12:24 +0200

    Commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on the homepage')
+})
+
+app.get('/posts', (req, res) => {
+    res.send('We are on the posts page')
+})
+
+//SERVER
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
